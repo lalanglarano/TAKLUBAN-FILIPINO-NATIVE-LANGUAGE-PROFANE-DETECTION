01fa802d35052b9ac4597649fb0fc42116559bf7
ANNALYN BELEN <annalynpelaez26@gmail.com>
2024-12-10 15:28:49 +0800

    - retrained the model for the wrong dataset path

--- PATTERN_GENERATION/bikol.py
+++ PATTERN_GENERATION/bikol.py
@@ -269,7 +269,7 @@ def main():
     pattern_generator.save_patterns_from_sentence(predefined_rules_path, sentence, "Profane sentence example")
     
     # Load your dataset
-    df = pd.read_csv('Results/dataset/Bikol_Dataset.csv')
+    df = pd.read_csv('Results/dataset/dataset_bikol.csv')
     
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(df['sentence'], df['profane'], test_size=0.5, random_state=48)
